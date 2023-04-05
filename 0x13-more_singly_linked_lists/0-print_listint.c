f8d1c5bf6499f6fff4e5f31378afb70a6df7f509
igwexxi <agbodaniel44@gmail.com>
2023-04-04 17:33:10 -0700

    prints element of listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node = 0;
+	size_t numb = 0;
 
 	while (h)
 	{
 	printf("%d\n", h->n);
-	node++;
+	numb++;
 	h = h->next;
 	}
 
-	return (node);
+	return (numb);
 }
